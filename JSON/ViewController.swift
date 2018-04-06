611e912f59aa1c024c1bfeb75a43ce2e4f5462cb
Shaun Tucker <tucke@shauns-air.home>
2018-04-06 17:59:46 -0400

    late first commit

--- JSON/ViewController.swift
+++ JSON/ViewController.swift
@@ -9,17 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = Bundle.main.url(forResource: "practice", withExtension: "json")
+        
+        if let url = url {
+            let data = NSData(contentsOf: url)
+            
+            if let data = data{
+                //print(data)
+                
+                do {
+                    
+                    let jsonObject = try JSONSerialization.jsonObject(with: data as Data, options: .allowFragments)
+                    
+                    //print(jsonObject)
+                    if let object = jsonObject as? [String: AnyObject] {
+                        if let allCasinos = object["casinos"] as? [[String: AnyObject]] {
+                            print(allCasinos)
+                        }
+                    }
+                    
+                } catch {
+                    
+                    print("error occurred")
+                }
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
